a1048e43e07b4c2e8cbfa2de38e44c232cf2471b
ProjectoOfficial <miniprojectsofficial@gmail.com>
2023-07-22 00:34:58 +0200

    fixed docker run.sh to mount src directory

--- docker/run.sh
+++ docker/run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Nome dell'immagine Docker
 IMAGE_NAME="devel"
 IMAGE_TAG="1.0.0"
 
-# Nome del container
 CONTAINER_NAME="ReinforcementProject"
 
-# Percorso assoluto del codice sorgente che desideri montare nel container
-# Assicurati di modificare "/path/to/your/src/folder" con il percorso corretto
 PATH_TO_SRC_FOLDER="~/github/ReinforcementLearningProject/src"
 
-# Esegui il container
+
+MOUNT_SRC_PATH="-v $(dirname $PWD)/src:/home/user/src"
+
+
 docker run  --shm-size 2GB -it --gpus all \
-                --name $CONTAINER_NAME \
-                --user $(id -u):$(id -g) \
-                $IMAGE_NAME:$IMAGE_TAG
+        ${MOUNT_SRC_PATH} \
+        -e http_proxy -e https_proxy \
+        -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) \
+        --name $CONTAINER_NAME \
+        --user $(id -u):$(id -g) \
+        --net='host' \
+        --group-add video \
+        $IMAGE_NAME:$IMAGE_TAG
 
 docker rm $CONTAINER_NAME
